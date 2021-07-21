5d2fa0bffea6eab92fea28dc9378c90ceacc0998
GYANAM-DEVELOPER <74140267+GYANAM-ui@users.noreply.github.com>
2021-07-21 23:48:13 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,31 @@
+// JavaScript functions
+
+function project1() {
+	window.open("https://7p08exzifrh9lahkvluqja-on.drv.tw/PROVID%20MEETI/");
+}
+
+function mail() {
+	window.open("mailto:ggyanam869@gmail.com?Subject=Hello Blogger");
+}
+
+function myFunction() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}
+
+var mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
